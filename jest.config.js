ad18c3ceb1cf31532d4d0667d4dec30442437de9
James <jamesistranho@gmail.com>
2022-03-12 21:33:14 -0300

    feat: add jest configs

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
