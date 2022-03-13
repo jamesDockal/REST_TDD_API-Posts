bbe2938ecc357bf4fc96b535f3ccfd442741a985
James <jamesistranho@gmail.com>
2022-03-13 02:01:26 -0300

    feat: add concatenation between posts and users

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import externalAPI from '../services/externalData'
+import mergeById from '../utils/mergeById'
 
 interface Post {
   userId: number
@@ -8,16 +9,52 @@ interface Post {
   body: string
 }
 
-interface APIResponse {
+interface User {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: {
+      lat: string
+      lng: string
+    }
+  }
+  phone: string
+  website: string
+  company: {
+    name: string
+    catchPhrase: string
+    bs: string
+  }
+}
+
+interface PostsResponse {
   data: Post[]
+}
+
+interface UsersResponse {
+  data: User[]
 }
 
 export default class PostRoutesController {
   async listAllPosts (req: Request, res: Response): Promise<Response> {
     try {
-      const { data } = await externalAPI.get('/posts')
+      const { data: posts }: PostsResponse = await externalAPI.get('/posts')
+      const { data: users }: UsersResponse = await externalAPI.get('/users')
 
-      return res.status(200).json(data)
+      const formattedPosts = posts.map(({ userId, ...rest }) => ({
+        ...rest,
+        id: userId
+      }))
+
+      const concatedPosts = mergeById(formattedPosts, users, 'user')
+
+      return res.status(200).json(concatedPosts)
     } catch (error) {
       return res.status(400).json({
         error: 'An error occurred to fetch posts'
@@ -34,16 +71,19 @@ export default class PostRoutesController {
         })
       }
 
-      const { data }: APIResponse = await externalAPI.get('/posts')
+      const { data: posts }: PostsResponse = await externalAPI.get('/posts')
+      const { data: users }: UsersResponse = await externalAPI.get('/users')
 
-      const filteredPosts = data.filter((post) => post.userId === userId)
+      const filteredPosts = posts.filter((post) => post.userId === userId)
       if (!filteredPosts.length) {
         return res.status(404).json({
           error: 'No posts found by user'
         })
       }
 
-      return res.status(200).json(filteredPosts)
+      const concatedPosts = mergeById(filteredPosts, users, 'user')
+
+      return res.status(200).json(concatedPosts)
     } catch (error) {
       return res.status(400).json({
         error: 'An error occurred to fetch posts'
--- src/routes/posts/posts.spec.ts
+++ src/routes/posts/posts.spec.ts
@@ -1,17 +1,6 @@
 import request from 'supertest'
 import app from '../../config/app'
 
-interface Post {
-  userId: number
-  id: number
-  title: string
-  body: string
-}
-
-interface Response {
-  body: Post[]
-}
-
 describe('Post Routes (List All) ', () => {
   it('should return status 200', async () => {
     await request(app).get('/api/posts').expect(200)
@@ -24,13 +13,13 @@ describe('Post Routes (List All) ', () => {
   })
 
   it('should post be in the right format', async () => {
-    const { body: sut }: Response = await request(app).get('/api/posts')
+    const { body: sut } = await request(app).get('/api/posts')
 
     sut.forEach((post) => {
-      expect(post).toHaveProperty('userId')
       expect(post).toHaveProperty('id')
       expect(post).toHaveProperty('title')
       expect(post).toHaveProperty('body')
+      expect(post).toHaveProperty('user')
     })
   })
 })
@@ -59,13 +48,13 @@ describe('Post Routes (List By User Id) ', () => {
   })
 
   it('should post be in the right format', async () => {
-    const { body: sut }: Response = await request(app).get('/api/posts/1')
+    const { body: sut } = await request(app).get('/api/posts/1')
 
     sut.forEach((post) => {
-      expect(post).toHaveProperty('userId')
       expect(post).toHaveProperty('id')
       expect(post).toHaveProperty('title')
       expect(post).toHaveProperty('body')
+      expect(post).toHaveProperty('user')
     })
   })
 })
